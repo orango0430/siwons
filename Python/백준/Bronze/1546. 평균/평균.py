eb54dbb604a376ebaaa403ca1404f355766aaec9
choisiwon <qm0430@naver.com>
2025-07-31 21:39:11 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1546. 평균/평균.py
+++ Python/백준/Bronze/1546. 평균/평균.py
@@ -1,5 +1,6 @@
-A=int(input())
-B=list (map(int,input().split()))
+import sys
+A=int(sys.stdin.readline().strip())
+B=list (map(int,sys.stdin.readline().split()))
 M=max(B)
 B=list(map(lambda x:x/M*100,B))
 total=sum(B)
