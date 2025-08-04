9fe7327a08a180076267013e5bd162a95f6650d7
choisiwon <qm0430@naver.com>
2025-08-04 18:28:00 +0900

    [Bronze III] Title: 웰컴 키트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ Python/백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -3,9 +3,6 @@ B=list(map(int,input().split()))
 T,P=map(int,input().split())
 result = 0
 for i in range(len(B)):
-    if B[i] % T == 0:
-        result+=B[i]//T
-    else:
-        result+=(B[i]//T)+1
+    result+=(B[i]+T-1)//T
 print(result)
 print(A//P, A%P)
