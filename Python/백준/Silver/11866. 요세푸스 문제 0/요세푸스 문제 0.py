c63758e73d8b22358b09d8a82b98c3d887895d8e
choisiwon <qm0430@naver.com>
2025-08-26 02:55:13 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 80 ms, Memory: 34908 KB -BaekjoonHub

---
+++ Python/백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,11 @@
+from collections import deque
+n, k=map(int, input().split())
+q=deque(range(1,n+1))
+result=[]
+while len(q) >= 1:
+    for _ in range(k-1):
+        q.append(q.popleft())
+    result.append(q.popleft())
+print('<',end='')
+print(*result,sep=', ',end='')
+print('>')
