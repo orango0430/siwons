83a48fd9a8b3bb67735aaeee27a122f89c45e9fe
choisiwon <qm0430@naver.com>
2025-08-27 16:55:21 +0900

    Update solved．ac.py

--- PyPy3/백준/Silver/18110. solved．ac/solved．ac.py
+++ PyPy3/백준/Silver/18110. solved．ac/solved．ac.py
@@ -7,7 +7,8 @@ if n == 0:
     print('0')
 else:
     result.sort()
-    m=my_round(n*0.15)
-    new=result[m:n-m]
-    avg=sum(new)/len(new)
+    m=my_round(n*0.15) # 직접구현 함수로 15% 계산
+    new=result[m:n-m] # 리스트에서 정렬 후 최소 15% 최대 15% 계산
+    avg=sum(new)/len(new) 
+
     print(my_round(avg))
