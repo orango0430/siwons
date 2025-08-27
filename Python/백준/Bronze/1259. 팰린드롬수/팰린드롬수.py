fbeaaf662b4e84463eaf26fe1c5f675759a0ebf9
choisiwon <qm0430@naver.com>
2025-08-27 16:58:48 +0900

    팰린드롬수.py

--- Python/백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ Python/백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -3,8 +3,9 @@ while True:
     n = input()
     if n == '0':
         break
-    if n == n[::-1]:
+    if n == n[::-1]: # 리스트로 입력받아서 뒤집는게 아니라 문자열로 뒤집는게 깔끔
         result.append('yes')
     else:
         result.append('no')
+
 print('\n'.join(result))
